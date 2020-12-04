1e02b5b3ef109c36dfedcccf21bde906fec98337
starry wisdom <cult.of.starry.wisdom@gmail.com>
2020-12-04 16:49:04 +0000

    Make memory_buffer header only

--- src/core-lib/memory_buffer.h
+++ src/core-lib/memory_buffer.h
@@ -91,6 +91,30 @@ template <size_t size> std::array<std::byte,size> pop_front_n(std::deque<std::by
 	return tmp;
 }
 
-void write_artemis_string(std::deque<std::byte>& queue, const std::string& string);
-std::string read_artemis_string_u8(std::deque<std::byte>& queue);
+//TODO needs test
+inline void write_artemis_string(std::deque<std::byte>& queue, const std::string& string) {
+	push_back<uint32_t>(queue,string.size()+1);
+	for (const auto& t : string) {
+		push_back<uint16_t>(queue,t);
+	}
+	push_back<uint16_t>(queue,0);
+}
+
+//TODO needs test
+inline std::string read_artemis_string_u8(std::deque<std::byte>& queue) {
+	std::string str;
+	auto count=pop_front<uint32_t>(queue);
+	bool skip{false};
+	for (uint32_t i=0; i!=count; i++) {
+		uint8_t chr=pop_front<uint16_t>(queue);
+		if (!skip) {
+			if (chr!=0) {
+				str+=chr;
+			} else {
+				skip=true;
+			}
+		}
+	}
+	return str;
+}
 }
